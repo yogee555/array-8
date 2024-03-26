ff687efa0a2410a5a60cd9a2fb10e7e161301b45
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-03-26 13:45:28 +0530

    Add files via upload

---
+++ array-8.py
@@ -0,0 +1,7 @@
+# Modification the array elements
+from array import*
+a= array('i',[10,20,30,40,50,60,70])
+print('character start to end',a[ : ])
+a[3]=45
+a[4]=55
+print('character from start to end after modification',a[ : ])
